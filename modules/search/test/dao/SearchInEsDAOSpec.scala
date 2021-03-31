cad49f03575f6843b5d88e327e5008177135b253
krishna <krishna.nature91@gmail.com>
2021-04-01 00:43:06 +0100

    Added more time for the test

--- modules/search/test/dao/SearchInEsDAOSpec.scala
+++ modules/search/test/dao/SearchInEsDAOSpec.scala
@@ -29,7 +29,7 @@ class SearchInEsDAOSpec
     with DockerKitDockerJava {
 
   // Github actions takes more than default 20 seconds to start containers
-  override val StartContainersTimeout: FiniteDuration = 5.minutes
+  override val StartContainersTimeout: FiniteDuration = 20.minutes
 
   implicit val pc: PatienceConfig = PatienceConfig(Span(20, Seconds), Span(1, Second))
 
