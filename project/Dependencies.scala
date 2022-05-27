47219499c8168102afe36c757880d01ac71ab866
krishna-thapa <krishna.nature91@gmail.com>
2022-05-27 17:38:07 +0100

    Fixed the Scala formatter

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,68 +4,73 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val playSlick       = "5.0.0"
-    val swagger         = "3.43.0"
-    val playjwt         = "5.0.0"
-    val bcrypt          = "4.3.0"
+    val playSlick = "5.0.0"
+    val swagger = "3.43.0"
+    val playjwt = "5.0.0"
+    val bcrypt = "4.3.0"
     val quartzScheduler = "1.8.5-akka-2.6.x"
-    val akkaVersion     = "2.6.19"
+    val akkaVersion = "2.6.19"
 
     // Database
-    val monogoDb   = "1.0.10-play28"
-    val elastic4s  = "7.9.1"
-    val playRedis  = "2.6.1"
+    val monogoDb = "1.0.10-play28"
+    val elastic4s = "7.9.1"
+    val playRedis = "2.6.1"
     val postgresql = "42.3.4"
 
     // tests
-    val scalaPlayTest  = "5.1.0"
+    val scalaPlayTest = "5.1.0"
     val testContainers = "0.40.7"
-    val mockitoVer     = "1.16.49"
+    val mockitoVer = "1.16.49"
 
     // logs
     val logbackEncoder = "6.6"
   }
 
   object Libraries {
-    val swaggerUi       = "org.webjars"       % "swagger-ui"             % Versions.swagger
-    val playJwt         = "com.pauldijou"     %% "jwt-play"              % Versions.playjwt
-    val scalaBcrypt     = "com.github.t3hnar" %% "scala-bcrypt"          % Versions.bcrypt
+    val swaggerUi = "org.webjars" % "swagger-ui" % Versions.swagger
+    val playJwt = "com.pauldijou" %% "jwt-play" % Versions.playjwt
+    val scalaBcrypt = "com.github.t3hnar" %% "scala-bcrypt" % Versions.bcrypt
     val quartzScheduler = "com.enragedginger" %% "akka-quartz-scheduler" % Versions.quartzScheduler
 
     // Akka
     lazy val akka = Seq(
-      "com.typesafe.akka"  %% "akka-stream"                % Versions.akkaVersion,
-      "com.typesafe.akka"  %% "akka-actor-typed"           % Versions.akkaVersion,
-      "com.typesafe.akka"  %% "akka-serialization-jackson" % Versions.akkaVersion,
-      "com.typesafe.akka"  %% "akka-slf4j"                 % Versions.akkaVersion,
-      "com.lightbend.akka" %% "akka-stream-alpakka-csv"    % "3.0.4",
-      "com.lightbend.akka" %% "akka-stream-alpakka-slick"  % "3.0.4"
+      "com.typesafe.akka" %% "akka-stream" % Versions.akkaVersion,
+      "com.typesafe.akka" %% "akka-actor-typed" % Versions.akkaVersion,
+      "com.typesafe.akka" %% "akka-serialization-jackson" % Versions.akkaVersion,
+      "com.typesafe.akka" %% "akka-slf4j" % Versions.akkaVersion,
+      "com.lightbend.akka" %% "akka-stream-alpakka-csv" % "3.0.4",
+      "com.lightbend.akka" %% "akka-stream-alpakka-slick" % "3.0.4"
     )
 
     // Reactive Mongo DB
     lazy val mongoDependencies = Seq(
-      "org.reactivemongo" %% "play2-reactivemongo"            % Versions.monogoDb,
+      "org.reactivemongo" %% "play2-reactivemongo" % Versions.monogoDb,
       "org.reactivemongo" %% "reactivemongo-play-json-compat" % Versions.monogoDb
     )
 
     // Slick for the Postgres DB
     lazy val slickDatabaseDependencies = Seq(
-      "org.postgresql"    % "postgresql"             % Versions.postgresql,
-      "com.typesafe.play" %% "play-slick"            % Versions.playSlick,
+      "org.postgresql" % "postgresql" % Versions.postgresql,
+      "com.typesafe.play" %% "play-slick" % Versions.playSlick,
       "com.typesafe.play" %% "play-slick-evolutions" % Versions.playSlick
     )
 
     // Elastic Search dependencies
     lazy val elastic4sDependencies = Seq(
       "com.sksamuel.elastic4s" %% "elastic4s-client-esjava" % Versions.elastic4s,
-      "com.sksamuel.elastic4s" %% "elastic4s-json-play"     % Versions.elastic4s,
-      "com.sksamuel.elastic4s" %% "elastic4s-http-streams"  % Versions.elastic4s
+      "com.sksamuel.elastic4s" %% "elastic4s-json-play" % Versions.elastic4s,
+      "com.sksamuel.elastic4s" %% "elastic4s-http-streams" % Versions.elastic4s
     )
 
-    val playRedis     = "com.github.karelcemus"  %% "play-redis"                     % Versions.playRedis
-    val scalaTestPlus = "org.scalatestplus.play" %% "scalatestplus-play"             % Versions.scalaPlayTest % "test"
-    val testContainer = "com.dimafeng"           %% "testcontainers-scala-scalatest" % Versions.testContainers % "test"
-    val mockitoSugar  = "org.mockito"            %% "mockito-scala-scalatest"        % Versions.mockitoVer % "test"
+    val playRedis = "com.github.karelcemus" %% "play-redis" % Versions.playRedis
+
+    val scalaTestPlus =
+      "org.scalatestplus.play" %% "scalatestplus-play" % Versions.scalaPlayTest % "test"
+
+    val testContainer =
+      "com.dimafeng" %% "testcontainers-scala-scalatest" % Versions.testContainers % "test"
+
+    val mockitoSugar = "org.mockito" %% "mockito-scala-scalatest" % Versions.mockitoVer % "test"
 
     // Docker test container
     lazy val testContainerDependencies = Seq(
@@ -89,5 +94,7 @@ object Dependencies {
         organization = "com.fasterxml.jackson.core"
       )
     )
+
   }
+
 }
